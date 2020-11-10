59a8fb3c73c92031e3a45d6671ffb7ab2120fdbf
brian926 <brian.antunes@yahoo.com>
2020-11-09 20:01:26 -0500

    Add server startup

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+
 import threading
 import socket
 import argparse
@@ -18,7 +19,7 @@ class Server(threading.Thread):
         sock.bind((self.host, self.port))
 
         sock.listen(1)
-        print('Listening at', sock.getsocketname())
+        print('Listening at', sock.getsockname())
 
         while True:
 
@@ -35,3 +36,60 @@ class Server(threading.Thread):
             # Add thread to active connection
             self.connections.append(server_socket)
             print('Ready to receive messages from', sc.getpeername())
+
+    def broadcast(self, message, source):
+
+        for connection in self.connections:
+
+            # Send to all connected clients except the source client
+            if connection.sockname != source:
+                connection.send(message)
+
+class ServerSocket(threading.Thread):
+
+    def __init__(self, sc, sockname, server):
+        super().__init__()
+        self.sc = sc
+        self.sockname = sockname
+        self.server = server
+
+    def run(self):
+
+        while True:
+            message = self.sc.recv(1024).decode('ascii')
+            if message:
+                print('{} says {!r}'.format(self.sockname, message))
+                self.server.broadcast(message, self.sockname)
+            else:
+                # Client has closed the socket, exit the thread
+                print('{} has closed the connection'.format(self.sockname))
+                self.sc.close()
+                self.server.remove_connection(self)
+                return
+
+    def send(self, message):
+        self.sc.sendall(message.encode('ascii'))
+
+def exit(server):
+
+    while True:
+        ipt = input('')
+        if ipt == 'q':
+            print('Closing all connections...')
+            for connection in server.connections:
+                connection.sc.close()
+            print('Shutting down the server...')
+            os._exit(0)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Chatroom Server')
+    parser.add_argument('host', help='Interface the server listens at')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060, help='TCP port (default 1060)')
+    args = parser.parse_args()
+
+    # Create and start server thread
+    server = Server(args.host, args.p)
+    server.start()
+
+    exit = threading.Thread(target = exit, args = (server,))
+    exit.start()
